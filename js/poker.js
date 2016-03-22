024355877527ab861c40bdaf09257d2f7df0df96
chanthebelch <michael-chan@outlook.com>
2016-03-22 22:33:00 +0800

    tweak

--- js/poker.js
+++ js/poker.js
@@ -2,19 +2,19 @@
  * Created by Michael on 3/21/2016.
  */
 var suits = ['diamond', 'club', 'heart', 'spade'];
-var cards = [['ace', [0,0,0,0,0], [0,0,3,0,0]],
-    ['two', [0,0,0,0,0], [1,0,0,0,5]],
-    ['three', [0,0,0,0,0], [1,0,3,0,5]],
-    ['four', [1,0,0,0,5], [0,0,0,0,0]],
-    ['five', [1,0,0,0,5], [0,0,3,0,0]],
-    ['six', [1,0,3,0,5], [0,0,0,0,0]],
-    ['seven', [1,0,3,0,5], [0,2,0,0,0]],
-    ['eight', [1,0,3,0,5], [0,2,0,4,0]],
-    ['nine', [1,2,3,4], [0,0,3,0,0]],
-    ['ten', [1,2,3,4], [0,2,0,4,0]],
-    ['jack', '&#9820'],
-    ['queen', '&#9819'],
-    ['king', '&#9818']];
+var cards = [['ace', 'A', [0,0,0,0,0], [0,0,3,0,0]],
+            ['two', '2', [0,0,0,0,0], [1,0,0,0,5]],
+            ['three', '3', [0,0,0,0,0], [1,0,3,0,5]],
+            ['four', '4', [1,0,0,0,5], [0,0,0,0,0]],
+            ['five', '5', [1,0,0,0,5], [0,0,3,0,0]],
+            ['six', '6', [1,0,3,0,5], [0,0,0,0,0]],
+            ['seven', '7', [1,0,3,0,5], [0,2,0,0,0]],
+            ['eight', '8', [1,0,3,0,5], [0,2,0,4,0]],
+            ['nine', '9', [1,2,3,4], [0,0,3,0,0]],
+            ['ten', '10', [1,2,3,4], [0,2,0,4,0]],
+            ['jack', 'J', '&#9820'],
+            ['queen', 'Q', '&#9819'],
+            ['king', 'K', '&#9818']];
 
 function inputACard() {}
 
@@ -38,6 +38,12 @@ function getAllCards() {
 function displayACard() {
     var suitIndex = Math.floor(Math.random()*4);
     var cardIndex = Math.floor(Math.random()*13);
+
+    var middle = $(this).find('.middle');
+    var edge = $(this).find('.edge');
+    var center = $(this).find('.center');
+    var cells = $(this).find('.middle>div>div');
+
     // remove a old suit class for .suit
     for (var i = 0; i < suits.length; i++) {
         $(this).removeClass(suits[i]);
@@ -47,30 +53,25 @@ function displayACard() {
         $(this).removeClass(cards[i][0]);
     }
 
+    // remove span tag from .middle
+    middle.find('span').remove();
+
     // add a new suit class for .suit and a new value class for .value
     $(this).addClass(suits[suitIndex]);
     $(this).addClass(cards[cardIndex][0]);
 
     // change the value for the card
-    //$(this).find('.value').text(cards[cardIndex][1]);
+    $(this).find('.value').text(cards[cardIndex][1]);
 
     // change the middle part of the card
-    var middle = $(this).find('.middle');
-    var edge = $(this).find('.edge');
-    var center = $(this).find('.center');
-    var cells = $(this).find('.middle>div>div');
-
     if (cardIndex < 10) {
-
-        // remove span tag from .middle
-        middle.find('span').remove();
 
         // remove v class for .middle>div>div
         cells.removeClass('v');
 
         // add a v class for the cells
-        var edgeContents = cards[cardIndex][1];
-        var centerContents = cards[cardIndex][2];
+        var edgeContents = cards[cardIndex][2];
+        var centerContents = cards[cardIndex][3];
         for (i = 0; i < edgeContents.length; i++) {
             edge.find('div:nth-child(' + edgeContents[i] + ')').addClass('v');
         }
@@ -79,10 +80,8 @@ function displayACard() {
         }
     }
     else {
-        // remove old span child
-        middle.find('span').remove();
         // add a new span child
-        middle.append('<span>' + cards[cardIndex][1] + '</span>');
+        middle.append('<span>' + cards[cardIndex][2] + '</span>');
     }
 }
 
